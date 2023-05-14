c58d448db60abd1969f960ff4afca2642096c4dc
Kafilat <kafilatadewumi61@gmail.com>
2023-05-14 10:51:47 +0100

    WIP

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../Footer'
+import Navbar from '../Navbar'
+import './Layout.css'
+
+const Layout = () => {
+  return (
+    <div className='layout-container'>
+      <Navbar/>
+      <main className='children-wrapper'>{children}
+      <Footer/>
+      </main>
+    </div>
+  )
+}
+
+export default Layout
