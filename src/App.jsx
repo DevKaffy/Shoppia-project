84cfa4524d46b1ea16c971f2915cfcb59ed7c7b2
Kafilat <kafilatadewumi61@gmail.com>
2023-05-17 16:27:48 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App'
-import Navbar from './Components/Navbar'
+import About from './Components/Pages/About';
+import Contact from './Components/Pages/Contact';
+import Home from './Components/Pages/Home';
+import SignUp from './Components/Pages/Signup';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+ const router = createBrowserRouter([
+   {
+     path: "/",
+     element: <Home />,
+   },
+   {
+     path: "/contact",
+     element: <Contact />,
+   },
+   {
+     path: "/about",
+     element: <About />,
+   },
+   {
+     path: "/signup",
+     element: <SignUp />,
+   },
+ ]);
+
+  return <RouterProvider router = {router} />
 }
+
 
 export default App
