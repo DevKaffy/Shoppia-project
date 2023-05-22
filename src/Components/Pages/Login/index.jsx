598dd4633537d588c3160f7d73a2b70f0fc4fb64
Kafilat <kafilatadewumi61@gmail.com>
2023-05-22 14:37:52 +0100

    WIP

--- src/Components/Pages/Login/index.jsx
+++ src/Components/Pages/Login/index.jsx
@@ -4,14 +4,17 @@ import Layout from '../../Layout'
 const Login = () => {
   return (
    <Layout>
-    <main>
-      <img src="/public/Side Image.png" alt="" />
-      <div>
+    <main className= "signup">
+      <img className='signup-img' src="/public/Side Image.png" alt="" />
+      <div className='right-container'>
     <h2>Log in to Shoppia</h2>
-    <p>Enter your details below</p>
-    <form>
+    <p className='detail'>Enter your details below</p>
+    <form className='form-wrapper'>
       <input type="text" placeholder='email' />
       <input type="text" placeholder='password'/>
+      <div>
+        
+      </div>
     </form>
       </div>
     </main>
