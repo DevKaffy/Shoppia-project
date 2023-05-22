97cdf903f0f018939378e4c6b0d45efbad59cd0f
Kafilat <kafilatadewumi61@gmail.com>
2023-05-22 13:11:30 +0100

    WIP

--- src/Components/Pages/Signup/index.jsx
+++ src/Components/Pages/Signup/index.jsx
@@ -13,15 +13,20 @@ const SignUp = () => {
           <h2>Create an account</h2>
           <p className="detail">Enter your details below</p>
           <form className="form-wrapper">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email or Phone Number" />
-            <input type="text" placeholder="Password" />
-            <button className="w-full bg-[#DB4444] pt-[1rem] pb-[1rem] rounded-[4px] text-[white] mt-[2.5rem]">Create Account</button>
+            <input type="text" placeholder="First name" required/>
+            <input type="text" placeholder="Last name" required/>
+            <input type="text" placeholder="Username" required/>
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Phone Number" required/>
+            <input type="text" placeholder="Password" required/>
+            <button className="w-full bg-[#DB4444] pt-[1rem] pb-[1rem] rounded-[4px] text-[white] mt-[2.5rem]">
+              Create Account
+            </button>
           </form>
-            <div className="google-container">
-              <img src="/public/Icon-Google.png" alt="" />
-              <p>Sign up with Google</p>
-            </div>
+          <div className="google-container">
+            <img src="/public/Icon-Google.png" alt="" />
+            <p>Sign up with Google</p>
+          </div>
         </div>
       </main>
     </Layout>
