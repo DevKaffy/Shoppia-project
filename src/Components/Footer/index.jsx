787234849e9b1aa6446b539c8b83e24030dd52d3
Kafilat <kafilatadewumi61@gmail.com>
2023-05-22 16:16:11 +0100

    WIP

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,15 +3,26 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='title'>
-      <span>Shoppia</span>
-      <span>Support</span>
-      <span>Account</span>
-      <span>Quick Link</span>
+    <div className="footer">
+      <div className="title">
+        <span className="text-[1.5rem]">Shoppia</span>
+        <span>Support</span>
+        <span>Account</span>
+        <span>Quick Link</span>
+      </div>
+      <div className="flex gap-x-[7rem]">
+        <span>Subscribe</span>
+        <span>
+          111 Bijoy sarani, Dhaka, <br /> DH 1515, Bangladesh.
+        </span>
+        <span>MY Account</span>
+        <span>Privacy Policy</span>
+      </div>
+      <div className="flex gap-x-[7rem]">
+        <span>Get 10% off your first order</span>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer
