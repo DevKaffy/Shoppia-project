1170cc54ab51d0a4903705dba849216730609ffe
Kafilat <kafilatadewumi61@gmail.com>
2023-05-26 16:16:01 +0100

    update

--- src/Components/Pages/Home/index.jsx
+++ src/Components/Pages/Home/index.jsx
@@ -37,6 +37,22 @@ const Home = () => {
             </div>
             <Category />
           </div>
+
+          <div className="categories">
+            <div className="flex gap-4 items-center mb-[1rem]">
+              <img src="/Rectangle 18.png" alt="" />
+              <div className="text-[#DB4444] font-semibold">This month</div>
+            </div>
+            <div className="flex items-center justify-between ">
+              <div className="text-[2.25rem] font-semibold">
+                Best Selling Products
+              </div>
+              <button className="bg-[#DB4444] pt-4 pb-4 pr-12 pl-12 rounded-[4px] text-white">
+                View All
+              </button>
+            </div>
+          </div>
+          <Category />
         </div>
       </main>
     </Layout>
