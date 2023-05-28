024655a7be686fc8aa43842ab75eec24b534e7aa
Kafilat <kafilatadewumi61@gmail.com>
2023-05-28 08:24:44 +0100

    update

---
+++ src/Components/BestSelling/BestSelling.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './BestSelling.css'
+
+const BestSelling = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default BestSelling
--- src/data.js
+++ src/data.js
@@ -83,3 +83,4 @@ export const categories = [
     title: "Gaming",
   },
 ];
+
