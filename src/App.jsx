40a0d281eb32becfaaaa8291cf82d238e7b6a819
WankioM <111064857+WankioM@users.noreply.github.com>
2023-05-30 09:54:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App'
+import About from './Components/Pages/About';
+import Contact from './Components/Pages/Contact';
+import Home from './Components/Pages/Home';
+import Login from './Components/Pages/Login';
+import Notfound from './Components/Pages/Notfound';
+import SignUp from './Components/Pages/Signup';
+import CheckOutPage from './Components/CheckOut/CheckOutPage'
+import ProductUpload from './Components/ProductUpload/UploadPage';
+import Test from './Components/Pages/UploadProduct/Test'
+
+
 const App = () => {
  const router = createBrowserRouter([
    {
@@ -30,6 +44,11 @@ const App = () => {
     path: '/productupload',
     component: <ProductUpload />,
     element: <ProductUpload />,
+  },
+
+  {
+    path: "/test",
+    element: <Test/>,
   },
   
    {
