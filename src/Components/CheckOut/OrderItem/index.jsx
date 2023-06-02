610f3b50c52f687b56ea2d42b8ca3f81d5c80d58
WankioM <111064857+WankioM@users.noreply.github.com>
2023-06-03 02:25:01 +0300

    Update index.jsx

--- src/Components/CheckOut/OrderItem/index.jsx
+++ src/Components/CheckOut/OrderItem/index.jsx
@@ -3,18 +3,39 @@ import React, { useState } from 'react'
 function OrderItem({item}) {
 
   const [quantity, setQuantity] = useState(item.quantity);
+  const [deleteRow, setDeleteRow] = useState(false);
 
   const incrementQuantity = () => {
-    setQuantity(quantity + 1);
-    console.log(quantity)
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+    console.log(newQuantity);
+    // Update the total based on the new quantity
+    const newTotal = item.price * newQuantity;
+    setTotal(newTotal);
   };
+  
 
   const decrementQuantity = () => {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
+    if (quantity > 1) {
+      const newQuantity = quantity - 1;
+    setQuantity(newQuantity);
+    console.log(newQuantity);
+    // Update the total based on the new quantity
+    const newTotal = item.price * newQuantity;
+    setTotal(newTotal);
+    setDeleteRow(false);
+
+    } else if (quantity === 1) {
+      // Display delete icon or perform delete action here
+      // For example, you can set a state variable to indicate deletion
+      setDeleteRow(true);
     }
   };
 
+
+    // Assuming you have a 'total' state variable
+    const [total, setTotal] = useState(item.price * item.quantity);
+  
 
   return (
     
@@ -22,14 +43,16 @@ function OrderItem({item}) {
     <tr >
     <td><img src={item.image} alt="Product Image" className="cart-image"/></td>
     <td>{item.name} <p className='desc'>{item.desc}</p></td>
-    <td>${item.price}</td>
+    <td>{item.price}</td>
     
-    <td>${item.price * item.quantity}</td>
+    <td>{total}</td>
     <td>
       <div className='incdec'>
-        <button className="invisible-button" ><i className="bi bi-plus-square-fill icon-style" onClick={incrementQuantity}></i></button>
+      <button className="invisible-button" >
+      {deleteRow ? <i className="bi bi-trash icon-style"></i> : <i className="bi bi-dash-square-fill icon-style"  onClick={decrementQuantity}></i>}
+      </button>
     <p className='quantity'>{quantity}</p>
-    <button className="invisible-button" ><i className="bi bi-dash-square-fill icon-style1" onClick={decrementQuantity}></i></button>
+    <button className="invisible-button" ><i className="bi bi-plus-square-fill icon-style" onClick={incrementQuantity}></i></button>
     </div>
     </td>
     </tr>
