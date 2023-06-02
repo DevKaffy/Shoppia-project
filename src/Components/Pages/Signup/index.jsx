2e87ed2721fd77ecec6727f7202f7c52b129ea65
Kafilat <kafilatadewumi61@gmail.com>
2023-06-02 13:58:59 +0100

    update

--- src/Components/Pages/Signup/index.jsx
+++ src/Components/Pages/Signup/index.jsx
@@ -5,8 +5,32 @@ import axios from 'axios';
 import './SignUp.css'
 
 const SignUp = () => {
-  const url = "";
-  const [signUp, setSignUp] = useState("")
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+  });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // API request
+    axios.post('https://shoppia-production.up.railway.app/api/v1/register', formData)
+    .then(response => {
+      // Handle the response
+      console.log(response.register);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+  };
   return (
     <Layout>
       <main className="signup">
@@ -17,13 +41,55 @@ const SignUp = () => {
           <h2>Create an account</h2>
           <p className="detail">Enter your details below</p>
           <form className="form-wrapper">
-            <input type="text" placeholder="First name" required />
-            <input type="text" placeholder="Last name" required />
-            <input type="text" placeholder="Username" required />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Phone Number" required />
-            <input type="text" placeholder="Password" required />
-            <button className="w-full bg-[rgb(219,68,68)] pt-[1rem] pb-[1rem] rounded-[4px] text-[white] mt-[2.5rem] mb-4">
+            <input
+              type="text"
+              placeholder="First name"
+              required
+              name="fistName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Last name"
+              required
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              required
+              name="userName"
+              value={formData.userName}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              required
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Phone Number"
+              required
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleInputChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            <button className="w-full bg-[rgb(219,68,68)] pt-[1rem] pb-[1rem] rounded-[4px] text-[white] mt-[2.5rem] mb-4" onClick={handleSubmit}>
               Create Account
             </button>
           </form>
