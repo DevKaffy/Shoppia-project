065016361963838e2f6acbd3db4aa0f511419e36
Kafilat <kafilatadewumi61@gmail.com>
2023-06-05 13:40:05 +0100

    update

--- src/Components/Pages/Contact/index.jsx
+++ src/Components/Pages/Contact/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Layout from '../../Layout';
+import './Contact.css'
 
 const Contact = () => {
   return (
     <Layout>
-      <main>
-        
+      <main className='contact'>
+        <div>
+          <div>Come back to me</div>
+          <div>ikrifkmdjnmcd</div>
+        </div>
+        <div className='input-container'>
+          <input type="text" placeholder='Your Name' />
+          <input type="text" placeholder='Your Email'/>
+          <input type="text" placeholder='Your Phone'/>
+        </div>
       </main>
     </Layout>
   );
