1b7b2da20a28aba1ee0528a160e9ec883460be93
Kafilat <kafilatadewumi61@gmail.com>
2023-06-05 15:42:16 +0100

    update

--- src/Components/Pages/Contact/index.jsx
+++ src/Components/Pages/Contact/index.jsx
@@ -7,18 +7,18 @@ const Contact = () => {
     <Layout>
       <main className="contact">
         <div>
-          <div  className='border-b pb-8'>
-            <div className="flex items-center gap-4">
+          <div className="border-b pb-8">
+            <div className="flex items-center gap-4 pb-4">
               <img src="/icons-phone.svg" alt="" />
-              <div>Call Us</div>
+              <div className="font-medium">Call Us</div>
             </div>
             <div>We are available 24/7, 7 days a week.</div>
             <div>Phone: +2348116206920</div>
           </div>
-          <div>
+          <div className='pt-8'>
             <div className="flex items-center gap-4">
               <img src="/public/icons-mail.svg" alt="" />
-              <div>Write To Us</div>
+              <div className='font-medium'>Write To Us</div>
             </div>
           </div>
         </div>
