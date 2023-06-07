85b2d7f3525170075f2eb40e525de547d74b98de
Kafilat <kafilatadewumi61@gmail.com>
2023-06-07 15:00:02 +0100

    update

--- src/Components/Pages/Notfound/index.jsx
+++ src/Components/Pages/Notfound/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Layout from '../../Layout'
+import { Link } from 'react-router-dom'
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
+    <Layout>
+      <main>
+    <div>
+      <h1>404 Not Found</h1>
+      <p>Your visited page not found. You may go to home page</p>
+      <button>
+        <Link to="/Home">Back to home page</Link>
+      </button>
+    </div>
+      </main>
+    </Layout>
   )
 }
 
