cda313d00fefd9f0527b9c321a4fa9572532c58f
Kafilat <kafilatadewumi61@gmail.com>
2023-06-07 16:00:07 +0100

    update

--- src/Components/Pages/Notfound/index.jsx
+++ src/Components/Pages/Notfound/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Layout from '../../Layout'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Layout from "../../Layout";
+import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
     <Layout>
       <main>
-    <div>
-      <h1>404 Not Found</h1>
-      <p>Your visited page not found. You may go to home page</p>
-      <button>
-        <Link to="/Home">Back to home page</Link>
-      </button>
-    </div>
+        <div className="mx-auto py-[5rem] w-[50%] flex flex-col items-center justify-center">
+          <h1 className="font-medium text-[6rem] mb-[1rem]">404 Not Found</h1>
+          <p className="mb-4">Your visited page not found. You may go to home page</p>
+          <button className="bg-[#DB4444] rounded-[5px] px-8 py-4">
+          <Link to="/">Back to home page</Link>
+          </button>
+        </div>
       </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default Notfound
+export default Notfound;
