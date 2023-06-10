bbe503b8210787e6319e502fb3e7d8bdb0748599
Kafilat <kafilatadewumi61@gmail.com>
2023-06-10 17:23:45 +0100

    update

--- src/Components/Pages/MyAccount/index.jsx
+++ src/Components/Pages/MyAccount/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Category from '../../Category/Category';
+import Sidebar from '../../Sidebar';
+import Products from '../../Products/Products';
 
 const MyAccount = () => {
   return (
@@ -10,7 +13,7 @@ const MyAccount = () => {
           <Link to="/">Home</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/about">About</Link>
-          <div className='cursor-pointer'>Logout</div>
+          <div className="cursor-pointer">Logout</div>
           <select
             className="p-[10px] border-none outline-none rounded-[5px]"
             name="campus"
@@ -32,6 +35,37 @@ const MyAccount = () => {
           <img src="/Cart1.svg" alt="" />
         </div>
       </div>
+      <main className="home">
+        <div className="top-items">
+          <Sidebar />
+          <img className="pl-[2.8rem] mb-4 w-full" src="/image.jpeg" alt="" />
+        </div>
+        <div className="main-container">
+          <div>
+            <div>
+              <div className="flex gap-4 items-center mb-[1rem]">
+                <img src="/Rectangle 18.png" alt="" />
+                <div className="text-[#DB4444] font-semibold">Products</div>
+              </div>
+              <div className="flex justify-end gap-4 mb-[2.5rem]">
+                <img src="/Fill With Left Arrow.png" alt="" />
+                <img src="/Fill with Right Arrow.png" alt="" />
+              </div>
+              <Products />
+            </div>
+          </div>
+          <div className="categories">
+            <div className="flex gap-4 items-center mb-[1rem]">
+              <img src="/Rectangle 18.png" alt="" />
+              <div className="text-[#DB4444] font-semibold">Categories</div>
+            </div>
+            <div className="text-[2.25rem] font-semibold mb-[3.75rem]">
+              Browse By Category
+            </div>
+            <Category />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
