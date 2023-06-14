bfbe85984ad8aa4cfb5de4273afb37242e10ed84
NDUBUISI MATTHEW MMADUABUCHUKWU <111059584+Ndumatt1@users.noreply.github.com>
2023-06-14 17:29:41 +0100

    Update ProductsItems.jsx to display database details

--- src/Components/Products/ProductsItems.jsx
+++ src/Components/Products/ProductsItems.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Products.css';
 
-const ProductsItems = ({ image, title, price, productId }) => {
+
+const ProductsItems = ({ image, title, price, productId, onAddToCart }) => {
   const [cartCount, setCartCount] = useState(0);
   const userId = localStorage.getItem('userId');
-  //const token = localStorage.getItem('token');
 
   const handleClick = async () => {
     try {
@@ -25,12 +25,13 @@ const ProductsItems = ({ image, title, price, productId }) => {
       alert(message);
 
       setCartCount(response.data.count);
+      onAddToCart(); // Call the onAddToCart function passed as a prop from Products component
     } catch (error) {
       console.error('Error adding to cart:', error);
     }
   };
 
-  const formattedPrice = `${price}`;
+  const formattedPrice = `₦${price}`;
 
   return (
     <div>
