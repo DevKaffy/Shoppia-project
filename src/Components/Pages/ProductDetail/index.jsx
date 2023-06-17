cd5b7e99f914c7d5dd62076250d2af99cc3f046d
Kafilat <kafilatadewumi61@gmail.com>
2023-06-17 02:44:49 +0100

    WIP: product detail

--- src/Components/Pages/ProductDetail/index.jsx
+++ src/Components/Pages/ProductDetail/index.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
     const {id}= useParams()
-    console.log(id)
+const [productDetail, setproductDetail] = useState("")
+    const fetchProduct = async () => {
+  try {
+    const response = await axios.get(
+      `https://shoppia-production.up.railway.app/api/v1/products/${id}`,
+  
+    );
+    const { data } = response.data;
+   setproductDetail(data);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+};
+  useEffect(() => {
+    fetchProduct();
+  }, [id]);
+
   return (
-    <div>ProductDetail</div>
-  )
+    <>
+    <div>
+        <img src={productDetail.imageUrl} alt="" />
+        <p>{productDetail.campus}</p>
+    </div>
+      <div>ProductDetail</div>
+    </>
+  );
 }
 
 export default ProductDetail
