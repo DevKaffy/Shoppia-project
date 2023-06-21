b4212ea413bfdc662a08413821e8f9b65f6ec6b4
Kafilat <kafilatadewumi61@gmail.com>
2023-06-21 16:45:42 +0100

    homepage-responsiveness

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <span>Account</span>
         <span>Quick Link</span>
       </div>
-      <div className="flex gap-x-[11rem]">
+      <div className="lg:flex lg:flex-row flex flex-col gap-x-[11rem] gap-y-4">
         <span>Subscribe</span>
         <span>
           111 Bijoy sarani, Dhaka 
@@ -18,13 +18,13 @@ const Footer = () => {
         <span>MY Account</span>
         <span>Privacy Policy</span>
       </div>
-      <div className="block lg:flex gap-x-[8rem]">
+      <div className="flex flex-col lg:flex lg:flex-row gap-x-[8rem] gap-y-4">
         <span>Get 10% off your first order</span>
         <span>shoppia@gmail.com</span>
         <span>Login/Register</span>
         <span>Terms of Use</span>
       </div>
-      <div className=" block lg:flex gap-x-[17rem] border-b pb-[2rem]">
+      <div className=" flex flex-col lg:flex lg:flex-row gap-x-[17rem] gap-y-4 border-b pb-[2rem]">
         <span>Cart</span>
         <span>Contact</span>
         <span>FAQ</span>
